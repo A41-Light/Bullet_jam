4583d8049311e44816f6684e730e25a8e0ce1aeb
A41-Light <khushalagarwalinslg@gmail.com>
2025-05-03 20:54:13 +0530

    Adding Enemy Script

---
+++ Bullet jam/Assets/Scripts/Player.cs
@@ -0,0 +1,51 @@
+using System.Numerics;
+using UnityEditor.ShaderGraph.Internal;
+using UnityEngine;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5f;
+
+    private Vector3 mousePos;
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0f; // Set z to 0 for 2D
+
+        Vector3 direction = mousePos - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = UnityEngine.Quaternion.Euler(0, 0, angle-90); // Rotate to face the mouse
+
+
+        ProcessInput();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void ProcessInput()
+    {
+        float moveX = Input.GetAxis("Horizontal") * speed;
+        float moveY = Input.GetAxis("Vertical") * speed;
+        moveInput = new Vector2(moveX, moveY);
+
+        
+    }
+
+    void Move()
+    {
+        rb.linearVelocity = moveInput;
+    }
+}
