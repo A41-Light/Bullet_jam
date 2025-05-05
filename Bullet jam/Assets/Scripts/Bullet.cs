420f43426c8771498a6b6502ece21bab41ba1363
A41-Light <khushalagarwalinslg@gmail.com>
2025-05-05 21:27:07 +0530

    added glow

--- Bullet jam/Assets/Scripts/Bullet.cs
+++ Bullet jam/Assets/Scripts/Bullet.cs
@@ -2,6 +2,13 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    Vector3 bottomLeft;
+    Vector3 topRight  ;
+    float screenLeft;
+    float screenRight;
+    float screenTop  ;
+    float screenBottom;
+
     public float speed = 10f; // Speed of the bullet
     public float lifetime = 2f; // Time before the bullet is destroyed
 
@@ -16,6 +23,12 @@ public class Bullet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        GetBounds(); // Get the screen bounds
+        if(CheckBounds())
+        {
+            Destroy(gameObject); // Destroy the bullet if it goes out of bounds
+        }
+
         // Destroy the bullet after a certain time
         Destroy(gameObject, lifetime);
     }
@@ -34,5 +47,24 @@ public class Bullet : MonoBehaviour
             // Destroy the enemy and the bullet
             Destroy(gameObject);
         }
+    }
+
+    void GetBounds()
+    {
+        bottomLeft = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0));
+        topRight   = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, 0));
+        screenLeft   = bottomLeft.x;
+        screenRight  = topRight.x;
+        screenTop    = topRight.y;
+        screenBottom = bottomLeft.y;
+    }
+
+    bool CheckBounds()
+    {
+        // Check if the bullet is out of bounds and destroy it
+        if (transform.position.x < screenLeft || transform.position.x > screenRight ||
+            transform.position.y < screenBottom || transform.position.y > screenTop)
+            return true;
+        return false;
     }
 }
--- Bullet jam/Assets/Scripts/Player.cs
+++ Bullet jam/Assets/Scripts/Player.cs
@@ -2,17 +2,26 @@
 using System.Numerics;
 using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 using Quaternion = UnityEngine.Quaternion;
 using Vector2 = UnityEngine.Vector2;
 using Vector3 = UnityEngine.Vector3;
 
 public class Player : MonoBehaviour
 {
+    Vector3 bottomLeft ;
+    Vector3 topRight  ;
+    float screenLeft;
+    float screenRight;
+    float screenTop;
+    float screenBottom;
+
     public float speed = 5f;
     public float mouseSensitivity = 5f;
     public GameObject bulletPrefab;
     public GameObject Turret1;
     public GameObject Turret2;
+    public Light2D light2D; // Reference to the Light2D component
     public float shootTimer = 0.05f; // Time between shots
     public float currentShootTimer = 0f; // Timer to track shooting cooldown
     public float offset = 0.5f; // Offset for bullet spawn position
@@ -31,6 +40,9 @@ public class Player : MonoBehaviour
     {
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Confined;
+        GetBounds();
+
+        light2D.intensity = Mathf.Sin(Time.time * 2f) * 1f + 2f;
 
         ProcessInput();
 
@@ -40,7 +52,9 @@ public class Player : MonoBehaviour
         Vector3 direction = mousePos - transform.position;
 
         angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0, 0, angle-90);
+        //transform.rotation = Quaternion.Euler(0, 0, angle-90);
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 0, angle - 90), Time.deltaTime * mouseSensitivity);
+
 
         if(currentShootTimer > 0f)
         {
@@ -69,6 +83,10 @@ public class Player : MonoBehaviour
     void Move()
     {
         rb.linearVelocity = moveInput;
+        Vector3 pos = transform.position;
+        pos.x = Mathf.Clamp(pos.x, screenLeft, screenRight);
+        pos.y = Mathf.Clamp(pos.y, screenBottom, screenTop);
+        transform.position = pos;
     }
 
     void Shoot()
@@ -79,5 +97,15 @@ public class Player : MonoBehaviour
             Instantiate(bulletPrefab, Turret2.transform.position, Quaternion.Euler(0, 0, angle));
             currentShootTimer = shootTimer; // Reset the shoot timer
         }
+    }
+
+    void GetBounds()
+    {
+        bottomLeft = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0));
+        topRight   = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, 0));
+        screenLeft   = bottomLeft.x;
+        screenRight  = topRight.x;
+        screenTop    = topRight.y;
+        screenBottom = bottomLeft.y;
     }
 }
